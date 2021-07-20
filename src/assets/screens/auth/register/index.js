6b060c70b96174b387cd141c7b3a20c36c150ecb
= <=>
2021-07-20 16:29:46 -0300

    Component do formulario de registro

--- src/assets/screens/auth/register/index.js
+++ src/assets/screens/auth/register/index.js
@@ -3,7 +3,7 @@ import Header from '../../../components/header'
 import { Column, Section, Title, Container, Card } from 'rbx'
 import LogoImage from '../../../images/logo.png'
 import "../../../../styles/auth.scss"
-
+import RegisterForm from "../../../components/auth/register_form"
 const RegisterScreen = () => (
     <Fragment>
         <Header />
@@ -27,6 +27,7 @@ const RegisterScreen = () => (
                                             </Title>
                                         </Column>
                                     </Column.Group>
+                                    <RegisterForm/>
                                 </Section>
                             </Card.Content>
                         </Card>
