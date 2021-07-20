20845862a94eb6123855b3a8cda27e9efa3f25cd
= <=>
2021-07-20 15:32:42 -0300

    Tela de Registro

--- src/assets/screens/auth/register/index.js
+++ src/assets/screens/auth/register/index.js
@@ -1,8 +1,39 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
+import Header from '../../../components/header'
+import { Column, Section, Title, Container, Card } from 'rbx'
+import LogoImage from '../../../images/logo.png'
+import "../../../../styles/auth.scss"
 
-const RegisterScreen = () =>(
+const RegisterScreen = () => (
     <Fragment>
-        Register Screen
+        <Header />
+        <Section size="medium" className="auth">
+            <Container>
+                <Column.Group centered>
+                    <Column size={3}>
+                        <Card>
+                            <Card.Content>
+                                <Section>
+                                    <Column.Group centered>
+                                        <Column size={12}>
+                                            <img alt="texto" src={LogoImage} />
+                                        </Column>
+                                    </Column.Group>
+
+                                    <Column.Group>
+                                        <Column size={12}>
+                                            <Title size={6} className="has-text-grey has-text-centered">
+                                                Your notes on the cloud
+                                            </Title>
+                                        </Column>
+                                    </Column.Group>
+                                </Section>
+                            </Card.Content>
+                        </Card>
+                    </Column>
+                </Column.Group>
+            </Container>
+        </Section>
     </Fragment>
 )
 
