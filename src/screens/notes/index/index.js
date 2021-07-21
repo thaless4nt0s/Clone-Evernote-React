85f6ecbbe6e0b5406da9e060c53d6dfedd8671d3
= <=>
2021-07-21 15:18:14 -0300

    Estrutura da pagina de notes e Header Mobile

--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react'
 import HeaderLogged from '../../../components/header_logged'
+import Notes from '../../../components/notes'
 function NotesScreen() {
     return (
         <Fragment>
             <HeaderLogged/>
-            Notes Screen
+            <Notes />
         </Fragment>
     )
 }
