677d1e41caa46821ef7ee6c7f4537e4ad62fe175
= <=>
2021-07-21 14:55:59 -0300

    Criando rotas Protegidas

--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,16 @@ import RegisterScreen from './screens/auth/register'
 import LoginScreen from './screens/auth/login'
 import NotesIndexScreen from './screens/notes/index'
 import UserEditScreen from "./screens/auth/users/edit"
+import PrivateRoute from './components/auth/private_route';
+
 const Routes = () =>(
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={HomeScreen}/>
             <Route exact path="/register" component={RegisterScreen} />
             <Route exact path="/login" component={LoginScreen}/>
-            <Route exact path="/notes" component={NotesIndexScreen} />
-            <Route exact path="/users/edit" component={UserEditScreen}/>
+            <PrivateRoute exact path='/notes' component={NotesIndexScreen}/>
+            <PrivateRoute exact path="/users/edit" component={UserEditScreen} />
         </Switch>
     </BrowserRouter>
 )
