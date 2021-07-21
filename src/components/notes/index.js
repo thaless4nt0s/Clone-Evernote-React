a5dbd01fe696779f4ffde875c8ed7a74e7687eff
= <=>
2021-07-21 15:23:49 -0300

    Estrutura da pagina de notes

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment, useEffect, useState } from 'react';
-
+import '../../styles/notes.scss'
 function Notes(props) {
     return (
         <Fragment>
             Notes...
+            <p class="notes">
+
+            </p>
         </Fragment>
     )
 }
